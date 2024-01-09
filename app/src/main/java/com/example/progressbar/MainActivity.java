37d5098e586e23a4796c6081f0ab92c8c570647a
lenovo <karguppibhushan@gmail.com>
2024-01-09 15:34:35 +0530

    progressBar01

---
+++ app/src/main/java/com/example/progressbar/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.progressbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+ProgressBar pg01;
+int progress = 0;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pg01 = (ProgressBar) findViewById(R.id.pg);
+
+
+
+        }
+    }
+
